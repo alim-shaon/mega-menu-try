563eb9c11be99864bea6b231fe4aaa71f1bd211c
Alim Imran Shaon <aishaon036@gmail.com>
2022-02-08 14:54:16 +0600

    try-2

--- test.js
+++ test.js
@@ -1,1 +1,15 @@
+// document.getElementById("dropId").addEventListener("mouseenter", (e) => {
+//   console.log("hii");
+//   const dropMenu = document.getElementById("dropMenu");
+//   dropMenu.classList.add("fix");
 
+//   const body = document.getElementById("bodyId");
+//   body.style.backgroundColor = "red";
+//   console.log(body);
+// });
+// document.getElementById("dropId").addEventListener("mouseout", (e) => {
+//   console.log("hii");
+//   const body = document.getElementById("bodyId");
+//     body.style.backgroundColor = "aquamarine";
+//     console.log(body);
+// });
